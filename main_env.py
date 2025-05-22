375e94e2717bf4c9cd1d0c4a2f86be2dfb0ff649
AdamSinale <adam.sinale@gmail.com>
2025-05-22 04:17:36 +0300

    .

--- main_env.py
+++ main_env.py
@@ -9,7 +9,7 @@ def main():
     load_dotenv()  # reads .env in current directory
     api_key = os.getenv("ABSTRACT_API_KEY")
     if not api_key:
-        raise ValueError("API key not found in environment.")
+        raise ValueError("API key not found in environment")
     fetch_data(api_key)
 
 if __name__ == "__main__":
